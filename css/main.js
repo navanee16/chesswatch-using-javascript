a0e56c7a4af67a06781e774c16d77d946523f156
Navanee <navaneekrish164@gmail.com>
2022-09-09 16:43:20 +0530

    chesswatch

---
+++ css/main.js
@@ -0,0 +1,19 @@
+var minA = document.getElementById("min1");
+// console.log(minA);
+var minB = document.getElementById("min2");
+// console.log(minB);
+var secA = document.getElementById("sec1");
+// console.log(secA);
+var secB = document.getElementById("sec2");
+// console.log(secB);
+var swap = document.getElementById("btn");
+// console.log(swap);
+var button1 = document.getElementById("strt");
+// console.log(button1);
+var button2 = document.getElementById("stp");
+// console.log(button2);
+
+
+
+
+
